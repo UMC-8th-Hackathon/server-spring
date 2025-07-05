54cdec5d627e22da155768931055daefd6ec71ba
geniusjun <geniusjun4663@gmail.com>
2025-07-06 00:29:58 +0900

    shop-find fix

--- src/main/java/com/umc/domain/shop/service/ShopServiceImpl.java
+++ src/main/java/com/umc/domain/shop/service/ShopServiceImpl.java
@@ -21,8 +21,8 @@ public class ShopServiceImpl implements ShopService {
     public List<ShopResponseDto> findNearbyShops(double latitude, double longitude) {
         // 위도 1도 ≈ 111km / 경도는 위도에 따라 다름 (서울 기준 대략 1도 ≈ 88km)
         // 대략 20km 반경을 위도/경도 기준으로 환산하면:
-        double latRange = 5.0; // 20km / 111km ≈ 0.18
-        double lngRange = 5.0; // 20km / 88km ≈ 0.23
+        double latRange = 4.0; // 20km / 111km ≈ 0.18
+        double lngRange = 4.0; // 20km / 88km ≈ 0.23
 
         int limit = 5;
 
